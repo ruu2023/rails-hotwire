0b9303cff91f892ea9622cfdcc9998c235f033fd
ruu2023 <kokihiru2038@gmail.com>
2025-10-26 18:29:59 +0900

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+  def sidebar_link_to(path, emoji, text)
+    classes = %w[my-1 nav-link text-white]
+    classes << "active" if current_page?(path)
+    link_to(path, class: clasess) do
+      tag.span(clas: "me-2") { emoji } + tag.span { text }
+    end
+  end
+
+  def icon(icon_name)
+    tag.i(class: ["bi", "bi-#{icon_name}"])
+  end
+
+  def icon_with_tet(icon_name, text)
+    tag.span(icon(icon_name), class: "me-2" + tag.span(text))
+  end
+end
