517e57432d9970211e627b5e75a11f7ca7c7bc74
ruu2023 <kokihiru2038@gmail.com>
2025-10-28 21:17:27 +0900

    bootstrap

--- Gemfile
+++ Gemfile
@@ -37,13 +37,15 @@ gem "kamal", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
-gem "kaminari"
+gem 'kaminari'
 
 gem "ransack"
 
 gem "dartsass-rails"
 
 gem "bootstrap", "~> 5.3.3"
+
+gem "bootstrap_form"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,13 @@ class Cat < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[id name age created_at updated_at]
   end
+
+  # 名前: 必須
+  validates :name, presence: true,length: { maximum: 10 }, # 例: 最大50文字
+      format: {
+        with: /\A[\p{hiragana}\p{katakana}ー－]+\z/,
+        message: "は、ひらがなまたはカタカナで入力してください"
+      }
+  # 年齢: 必須 + integer + 0 以上
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
